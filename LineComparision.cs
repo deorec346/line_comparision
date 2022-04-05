2db9330f2a9b528f9a537b1f9287eea569d70033
[chetan deore] <[deorec346@gmail.com]>
2022-04-05 23:16:53 +0530

    Oops Concept

---
+++ LineComparision.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineCompProblem.LineComparisionProb
+{
+    
+       //Using Java object oriented programming concepts-
+        internal class LineComparision
+        {
+
+            public int X_One;
+            public int X_Two;
+            public int Y_One;
+            public int Y_Two;
+            public int S_One;
+            public int S_Two;
+            public int R_One;
+            public int R_Two;
+            public double Length_XY;
+            public double Length_SR;
+            public void length_line()
+            {
+
+                Console.WriteLine("Enter x, y co ordinates of the line 1 :");
+                Console.WriteLine("Enter value X_One");
+                X_One = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter vale X_Two");
+                X_Two = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter vale Y_One");
+                Y_One = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter vale Y_Two");
+                Y_Two = Convert.ToInt32(Console.ReadLine());
+
+
+                Console.WriteLine("Enter x, y co ordinates of the line 2 :");
+                Console.WriteLine("Enter value of S_One");
+                S_One = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter value of S_Two");
+                S_Two = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter vale R_One");
+                R_One = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter vale R_Two");
+                R_Two = Convert.ToInt32(Console.ReadLine());
+
+                double Length_XY = Math.Sqrt(Math.Pow((X_One - X_Two), 2) + Math.Pow((Y_Two - Y_One), 2));
+                Console.WriteLine("Length of Line 1 is:: " + Length_XY);
+
+                double Length_SR = Math.Sqrt(Math.Pow((S_Two - S_One), 2) + Math.Pow((R_Two - R_One), 2));
+                Console.WriteLine("Length of Line 2 is: " + Length_SR);
+                Console.ReadLine();
+
+            }
+            public void function_check()
+            {
+                bool equal_result = Length_XY.Equals(Length_SR);
+
+                if (equal_result == false)
+                    Console.WriteLine(" Length of XY and Length of SR are NOT Equal");
+                else
+                    Console.WriteLine("Length of XY and Length of SR are Equal");
+                Console.ReadKey();
+            }
+
+            public void Comparelines()
+            {
+                double Differene = Length_XY.CompareTo(Length_SR);
+                if (Differene < 0)
+                {
+                    Console.WriteLine("Length of XY is less than Length of SR");
+                }
+                if (Differene > 0)
+                {
+                    Console.WriteLine("Length of XY is greater than Length of SR");
+                }
+                if (Differene == 0)
+                {
+                    Console.WriteLine("Length of XY is equal to Length of SR");
+                }
+                Console.ReadKey();
+            }
+        }
+    
+}    
+    
+
+
+
+
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineCompProblem.LineComparisionProb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Problems");
+            Console.ReadLine();
+
+            LineComparision lineComparision = new LineComparision();
+            lineComparision.length_line();
+            lineComparision.function_check();
+            lineComparision.Comparelines();
+        }
+    }
+}
